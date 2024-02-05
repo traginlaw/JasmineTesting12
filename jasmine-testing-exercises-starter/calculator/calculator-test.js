8757bebe0127e00647b76a05d5dcb55a97c43642
traginlaw <traginlaw@gmail.com>
2024-02-04 19:37:49 -0600

    needs finished

---
+++ jasmine-testing-exercises-starter/calculator/calculator-test.js
@@ -0,0 +1,17 @@
+it('should calculate the monthly rate correctly', function () {
+	const values = {
+		amount: 10000,
+		years: 10,
+		rate: 2.5,
+	};
+	expect(calculateMonthlyPayment(values)).toEqual('94.27');
+});
+
+it('should return a result with 2 decimal places', function () {
+	const values = {
+		amount: 10000,
+		years: 10,
+		rate: 2.5,
+	};
+	expect(calculateMonthlyPayment(values)).toEqual('94.27');
+});
---
+++ jasmine-testing-exercises-starter/calculator/calculator.js
@@ -0,0 +1,58 @@
+window.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('calc-form');
+	if (form) {
+		setupIntialValues();
+		form.addEventListener('submit', function (e) {
+			e.preventDefault();
+			update();
+		});
+	}
+});
+
+function getCurrentUIValues() {
+	return {
+		amount: +document.getElementById('loan-amount').value,
+		years: +document.getElementById('loan-years').value,
+		rate: +document.getElementById('loan-rate').value,
+	};
+}
+
+// Get the inputs from the DOM.
+// Put some default values in the inputs
+// Call a function to calculate the current monthly payment
+function setupIntialValues() {
+	const amount = document.getElementById('loan-amount').value;
+	const years = document.getElementById('loan-years').value;
+	const rate = document.getElementById('loan-rate').value;
+	const userValues = { amount: 0, years: 0, rate: 0.0 };
+	const amountUser = userValues.amount;
+	const yearsUser = userValue.years;
+	const rateUsers = userValues.rate;
+	update();
+}
+
+// Get the current values from the UI
+// Update the monthly payment
+function update() {
+	const currentUserValues = getCurrentUIValues();
+	updateMonthly(calculateMonthlyPayment(currentUserValues));
+}
+
+// Given an object of values (a value has amount, years and rate ),
+// calculate the monthly payment.  The output should be a string
+// that always has 2 decimal places.
+function calculateMonthlyPayment(values) {
+	const interest = (values.rate * 0.01) / 12;
+	const months = Math.floor(values.years * 12);
+	return (
+		(interest * values.amount) /
+		(1 - Math.pow(1 + interest - months))
+	).toFixed(2);
+}
+
+// Given a string representing the monthly payment value,
+// update the UI to show the value.
+function updateMonthly(monthly) {
+	const monthlyAmount = document.getElementById('monthly-payment');
+	monthlyAmount.innerText = `$${monthly}`;
+}
